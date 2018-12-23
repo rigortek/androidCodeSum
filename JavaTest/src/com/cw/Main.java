ced402cb33bc350dc4c61fbbb84b7fcf2a889b94
robinjiang <robinjiang@china-liantong.com>
2018-12-23 23:49:41 +0800

    add intellij idea pure java proj: JavaTest

---
+++ JavaTest/src/com/cw/Main.java
@@ -0,0 +1,33 @@
+package com.cw;
+
+import java.util.Random;
+
+// https://www.jetbrains.com/idea/download/#section=linux
+// https://blog.csdn.net/qq_35246620/article/details/80522720
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(getRequestId(16));
+    }
+
+
+    public static String getRequestId(int strlen) {
+        char[] chr = {'0','a','A'};
+        Random random = new Random(System.currentTimeMillis());
+        StringBuffer sb = new StringBuffer();
+        int r = 0;
+        for (int i = 0; i < strlen; i++) {
+            r = Math.abs(random.nextInt());
+            if (0 == (r % 3)) {
+                sb.append((char)(chr[r % 3] + Math.abs(random.nextInt()) % 10));
+            } else {
+                sb.append((char)(chr[r % 3] + Math.abs(random.nextInt()) % 26));
+            }
+        }
+        return sb.toString();
+    }
+}
+
+
--- PureJavaProj/src/main/java/jcw/TestFinal.java
+++
@@ -1,5 +0,0 @@
-package jcw;
-
-public class TestFinal {
-    public int mAge;
-}
